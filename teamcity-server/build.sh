e4b1cae4ca95639aa95ef21212b899d3d94cdcd6
stephen.zambaux <stephen.zambaux@gmail.com>
2018-01-15 17:15:23 +0100

    Add deployment inside build.sh

--- teamcity-server/build.sh
+++ teamcity-server/build.sh
@@ -3,4 +3,5 @@
 version=1.0.6
 projectId=xxxx-xx
 docker build -t gcr.io/xxx-xx/teamcity-server:$version .
-gcloud docker -- push gcr.io/$projectId/teamcity-server:$version 
+gcloud docker -- push gcr.io/$projectId/teamcity-server:$version
+kubectl set image deployments/teamcity-server teamcity-server=gcr.io/$projectId/teamcity-server:$version
